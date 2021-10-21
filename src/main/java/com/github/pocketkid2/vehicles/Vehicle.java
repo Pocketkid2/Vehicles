64c8e4601a5dcc296dfd1276d8638a44e50eacb8
Adam Taylor <afranklint16@gmail.com>
2021-10-21 09:22:45 -0600

    Create base vehicle class

---
+++ src/main/java/com/github/pocketkid2/vehicles/Vehicle.java
@@ -0,0 +1,19 @@
+package com.github.pocketkid2.vehicles;
+
+import java.util.List;
+
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+
+public class Vehicle {
+
+	private List<Block> blocks;
+
+	private Player pilot;
+
+	public Vehicle(Player p, List<Block> b) {
+		pilot = p;
+		blocks = b;
+	}
+
+}
