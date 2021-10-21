2c985db334f5603bb973b98b7e7e878a04d58bbb
Adam Taylor <afranklint16@gmail.com>
2021-10-21 09:23:00 -0600

    Create a listener
    Will probably need to be split up relatively quickly

---
+++ src/main/java/com/github/pocketkid2/vehicles/VehiclesListener.java
@@ -0,0 +1,12 @@
+package com.github.pocketkid2.vehicles;
+
+import org.bukkit.event.Listener;
+
+public class VehiclesListener implements Listener {
+
+	private VehiclesPlugin plugin;
+
+	public VehiclesListener(VehiclesPlugin p) {
+		plugin = p;
+	}
+}
