1bf2c6ab2e1e75bb6caff1feab0d63d63cb8cda5
Adam Taylor <afranklint16@gmail.com>
2021-11-15 12:51:37 -0700

    Started work on density config file
    It will contain a list of blocks according to the spigot API names, followed by their density (either as an integer or a double, haven't decided which yet)

--- src/main/java/com/github/pocketkid2/vehicles/VehiclesPlugin.java
+++ src/main/java/com/github/pocketkid2/vehicles/VehiclesPlugin.java
@@ -1,16 +1,69 @@
 package com.github.pocketkid2.vehicles;
 
+import java.io.File;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class VehiclesPlugin extends JavaPlugin {
 
+	// Density config fields
+	private File densitiesFile;
+	private FileConfiguration densitiesConfig;
+
 	@Override
 	public void onEnable() {
+
+		// Create config.yml if it doesn't exist, and load it
+		saveDefaultConfig();
+
+		// Create densities.yml if it doesn't exist, and load it
+		createDensitiesConfig();
+
+		// Tell the console we've finished enabling the plugin
 		getLogger().info("Enabled!");
 	}
 
 	@Override
 	public void onDisable() {
 		getLogger().info("Disabled!");
+	}
+
+	//
+	// This method simply returns the configuration class instance so that other
+	// classes in this plugin can have access to it. This could potentially also be
+	// used for an API.
+	//
+	public FileConfiguration getDensitiesConfig() {
+		return densitiesConfig;
+	}
+
+	//
+	// This method creates the density config file if it doesn't exist, and loads
+	// the configuration into memory if it does
+	//
+	public void createDensitiesConfig() {
+
+		// First create the path
+		densitiesFile = new File(getDataFolder(), "densities.yml");
+
+		// If the file hasn't been created yet, do so
+		if (!densitiesFile.exists()) {
+
+			// Make sure the folder exists (possibly redundant?) and write the file
+			densitiesFile.getParentFile().mkdirs();
+			saveResource("densities.yml", false);
+		}
+
+		// Create a new configuration class for YAML files
+		densitiesConfig = new YamlConfiguration();
+
+		// Read it, and if an error, print to console so we can debug
+		try {
+			densitiesConfig.load(densitiesFile);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
